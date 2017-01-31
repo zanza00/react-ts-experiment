0b8a9b6f97c1cb6a91201b0ef5dbfcbd641405aa
Simone Picciani <simone.picciani@spindox.it>
2017-01-31 11:05:59 +0100

    top heroes is now almost dynamic

--- src/containers/hall-fame-container/index.tsx
+++ src/containers/hall-fame-container/index.tsx
@@ -3,14 +3,18 @@ import { PageSection } from '../../components/page-section';
 import { PageHero } from '../../components/page-hero';
 
 export function HallFameContainer() {
+  const topHeroes = [
+    { name: 'aaaaa', votes: 123, heroPage: '#' },
+    { name: 'bbbbb', votes: 234, heroPage: '#' },
+    { name: 'ccccc', votes: 342, heroPage: '#' },
+    { name: 'ddddd', votes: 42, heroPage: '#' },
+  ]
   return (
     <article>
       <PageHero title="Hall of Fame" subtitle="Most voted heroes" />
       <PageSection className="o-container o-container--small">
         <ol>
-          <li>
-            <a href="#">Hero Page</a> - 350 votes
-          </li>
+          {topHeroes.map(hero => <li><a href={hero.heroPage}>{hero.name}</a> {hero.votes}</li>)}
         </ol>
         <br />
       </PageSection>
